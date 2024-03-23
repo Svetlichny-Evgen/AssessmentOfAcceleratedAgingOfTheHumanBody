373edc16edb4e71dafca63579d85ad3cd9b3d96b
Svetlichny-Evgen <svetlichnyi.e@gmail.com>
2024-03-23 23:52:33 +0200

    Add project files.

---
+++ AssessmentOfAcceleratedAgingOfTheHumanBody1/Models/Questions/OptionsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AssessmentOfAcceleratedAgingOfTheHumanBody1.Models.Questions
+{
+    public class OptionsModel
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ AssessmentOfAcceleratedAgingOfTheHumanBody1/Models/Questions/QuestionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AssessmentOfAcceleratedAgingOfTheHumanBody1.Models.Questions
+{
+    public class QuestionModel
+    {
+        public string Id { get; set; }
+        public string? Text { get; set; }
+        public List<OptionsModel>? Options { get; set; }
+        public string? SelectedOption { get; set; }
+    }
+}
